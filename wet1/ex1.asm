61d6d35c12cc5ddf22e8260789e0fcb7e33d4972
itamararbiv <103099905+itamararbiv@users.noreply.github.com>
2022-04-06 19:14:48 +0300

    Add files via upload

--- wet1/ex1.asm
+++ wet1/ex1.asm
@@ -8,25 +8,21 @@ CountBits: .zero 4
 main:
 mov $0x00000001, %rax # rax = 0x00000001
 mov $64, %rbx # rbx = 64 (amount of loops)
-mov $0x0, %rcx
+mov $0x0, %ecx
+mov (num), %rdx
 Loop1: 
+    mov $0x00000001, %rax 
     cmp $0x0, %rbx
     jle end_Loop
-    and (num), %rax
-    shr %rax
-    movq $0x1, %rax
+    and %rdx, %rax
+    shr %rdx
+    cmp $0x1, %rax
     jge DO1 
 Back:
     dec %rbx
     jmp Loop1 
 DO1: 
-    inc %rcx
+    inc %ecx
     jmp Back
 end_Loop: 
-    
-     
-       
-    
-    
-    
-  
+    movl %ecx, (CountBits)
