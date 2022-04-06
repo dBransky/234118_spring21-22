dead9a2d7183b70904765d94d613f2f6f50ff2ad
itamararbiv <103099905+itamararbiv@users.noreply.github.com>
2022-04-06 18:55:07 +0300

    Add files via upload

--- wet1/ex1.asm
+++ wet1/ex1.asm
@@ -1,5 +1,32 @@
-.global _start
+.global main
 
-.section .text
-_start:
-#your code here
+.data 
+num: .quad 0x202
+CountBits: .zero 4
+
+.text
+main:
+mov $0x00000001, %rax # rax = 0x00000001
+mov $64, %rbx # rbx = 64 (amount of loops)
+mov $0x0, %rcx
+Loop1: 
+    cmp $0x0, %rbx
+    jle end_Loop
+    and (num), %rax
+    shr %rax
+    movq $0x1, %rax
+    jge DO1 
+Back:
+    dec %rbx
+    jmp Loop1 
+DO1: 
+    inc %rcx
+    jmp Back
+end_Loop: 
+    
+     
+       
+    
+    
+    
+  
