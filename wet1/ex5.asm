4d1f182792597b592597b50d4f66cf8b01a6dcf3
dBransky <79902268+dBransky@users.noreply.github.com>
2022-04-07 14:36:39 +0300

    Add files via upload
    finished, need to run more tests

--- wet1/ex5.asm
+++ wet1/ex5.asm
@@ -1,5 +1,56 @@
-.global _start
+.global main
+.data
+C: .long 3
+    .quad 0
+B: .long 2
+    .quad C
+A: .long 1
+    .quad B
+head: .quad A
+src: .long 4
+dst: .long 4
 
 .section .text
-_start:
-#your code here
+main:
+    movl (head),%eax
+    xor %ebx,%ebx
+    xor %ecx,%ecx
+loop_start:
+    movl (%eax),%edx
+    jmp search_src
+next_node:
+    mov 4(%eax),%eax
+    cmp $0,%eax
+    je end_of_array
+    jmp loop_start
+    
+end_of_array:
+    cmp $0,%ebx
+    je finish
+    cmp $0,%ecx
+    je finish
+    movl (%ebx),%r8D
+    movl (%ecx),%r9D
+    movl %r8D,(%ecx)
+    movl %r9D,(%ebx)
+    jmp finish
+    
+search_src:
+    cmp %edx,(src)
+    je found_src
+search_dst:    
+    cmp %edx,(dst)
+    je found_dst
+    jmp next_node
+found_src:
+    cmp $0,%ebx
+    jne finish
+    movl %eax,%ebx
+    jmp search_dst
+found_dst:
+    cmp $0,%ecx
+    jne finish
+    movl %eax,%ecx
+    jmp next_node
+finish:
+   jmp finish
