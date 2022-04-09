fb7467fdc95f65ae104a313d54fe105651536b6b
itamararbiv <103099905+itamararbiv@users.noreply.github.com>
2022-04-09 09:33:57 +0300

    Add files via upload

--- wet1/ex2.asm
+++ wet1/ex2.asm
@@ -1,5 +1,33 @@
-.global _start
+.global main
+
+.data
+destination: .quad 0x60000
+source: .quad 0x50000
+
+num: .long 0x5
 
 .section .text
-_start:
-#your code here
+main:
+    cmp $0, (num)
+    jg MEM_MOVE
+    jmp REV_MEM_MOVE
+MEM_MOVE:
+    movq $destination, %rbx
+    movq $source, %rcx
+    movl (num), %eax
+    jmp LOOP1
+REV_MEM_MOVE:
+    movq $destination, %rcx
+    movq $source, %rbx
+    imul $-1, (num), %eax
+    jmp LOOP1
+LOOP1:
+    cmp $0, %eax
+    je END
+    movb (%rcx), %dl
+    movb %dl, (%rbx)
+    add $1, %rbx
+    add $1, %rcx
+    dec %eax 
+END:
+    
