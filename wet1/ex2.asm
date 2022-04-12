ad12329641789418ffd2af9d0374685c58184c57
itamararbiv <103099905+itamararbiv@users.noreply.github.com>
2022-04-12 18:10:59 +0300

    Update ex2.asm

--- wet1/ex2.asm
+++ wet1/ex2.asm
@@ -1,44 +1,62 @@
-.global _start
+.global main
+
+.section .data
+source: .short 0xCAFE
+destination: .quad 0xAABBCCDD
+num: .long 0x3
 
 
 .section .text
-_start:
-#your code here
-    movq $0x0, %rax
-    movl (num), %eax
-    cmp $0, %eax
-    jge CONTINUE1_HW1
-    imul $-1, %eax
-    jmp CONTINUE2_HW1
-CONTINUE1_HW1:
+main:
+    # write your code here
+    movq $source, %rax
+    movq $destination, %rbx
     movq $0x0, %rcx
-    addl %eax, %ecx
-    addq $source, %rcx
-    addq $-1, %rcx
-    #destination
-    movq $0x0, %rbx
-    addl %eax, %ebx
-    addq $destination, %rbx
-    addq $-1, %rbx
-    jmp LOOP_HW1
-CONTINUE2_HW1:
-    #source
-    movq $0x0, %rbx
-    addl %eax, %ebx
-    addq $source, %rbx
-    addq $-1, %rbx
-    #destination
-    movq $0x0, %rcx
-    addl %eax, %ecx
-    addq $destination, %rcx
-    addq $-1, %rcx
-LOOP_HW1:
-    cmp $0x0, %eax
-    je EXIT
-    movb (%rcx), %dl
+    movl (num), %ecx
+    cmp $0x0, %ecx
+    jg POSITIVE_HW1
+    imul $-1, %ecx
+    movq $source, %rbx
+    movq $destination, %rax
+POSITIVE_HW1:
+    movq %rax, %rsi
+    movq %rbx, %rdi
+    addl %ecx, %esi
+    addl %ecx, %edi
+    # rdi = destination + num
+    # rsi = source + num
+    cmp %rbx, %rsi
+    jg IF2_HW1
+    jmp FRONT_LOOP_HW1
+IF2_HW1:
+    cmp %rax, %rbx
+    jg MY_UPDATE_HW1
+    jmp FRONT_LOOP_HW1
+MY_UPDATE_HW1:
+    sub $1, %rsi
+    sub $1, %rdi
+BACK_LOOP_HW1:
+    cmp $0x0, %ecx
+    je EXIT_HW1
+    movb (%rsi), %dl
+    movb %dl, (%rdi)
+    dec %rsi
+    dec %rdi
+    dec %ecx
+    jmp BACK_LOOP_HW1
+FRONT_LOOP_HW1:
+    cmp $0x0, %ecx
+    je EXIT_HW1
+    movq $0x0, %rdx
+    movb (%rax), %dl
     movb %dl, (%rbx)
-    dec %rcx
-    dec %rbx
-    dec %eax
-    jmp LOOP_HW1
-EXIT:
+    inc %rax
+    inc %rbx
+    dec %ecx
+    jmp FRONT_LOOP_HW1
+EXIT_HW1:
+    
+            
+
+
+
